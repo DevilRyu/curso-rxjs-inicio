a2bcee867b06f030658dab7f7b4958441e770f09
Diego Munoz <diego.munoz@kushkipagos.com>
2021-10-23 10:40:20 -0500

    distinctUntilKeyChanged Emite objetos siempre y cuando el anterior no tenga el mismo valor en una propiedad especifica

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,8 @@
-import {Observable, Observer, from} from 'rxjs';
-import {distinctUntilChanged} from 'rxjs/operators';
+import {Observable, Observer} from 'rxjs';
+import {} from 'rxjs/operators';
 
 const observer: Observer<any> = {
     next: (value: any) => console.log('next: ', value),
     error: null,
     complete: () => console.info('complete')
 };
-
-interface Personaje {
-    nombre: string;
-}
-
-const personajes: Personaje[] = [
-    {
-        nombre: 'Megaman'
-    },
-    {
-        nombre: 'Megaman'
-    },
-    {
-        nombre: 'Zero'
-    },
-    {
-        nombre: 'Dr. Willy'
-    },
-    {
-        nombre: 'X'
-    },
-    {
-        nombre: 'X'
-    },
-    {
-        nombre: 'Zero'
-    }
-]
-
-from(personajes).pipe(
-    distinctUntilChanged( (ant:Personaje, act:Personaje) => ant.nombre === act.nombre)
-).subscribe(console.log);
