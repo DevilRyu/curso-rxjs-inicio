7d21b9f32b5298bc093df7cd75df313b6b636071
Diego Munoz <diego.munoz@kushkipagos.com>
2021-10-24 19:51:21 -0500

    Sexto y ultimo ejercicio

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import {of, interval, Observable, Observer, combineLatestWith, from, forkJoin} from 'rxjs';
-import {ajax} from 'rxjs/ajax';
-import {catchError, delay, map} from "rxjs/operators";
-
-const observer: Observer<any> = {
-    next: (value: any) => console.log('next: ', value),
-    error: (err: any) => console.warn('error en subs: ', err),
-    complete: () => console.info('complete')
-};
